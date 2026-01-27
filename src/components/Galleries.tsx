53ca56a2cd29eab4e80df6d51f2a512fcb931188
jackson-zablon15 <jacksonzabron15@gmail.com>
2026-01-27 12:41:06 +0300

    Updating gallery

--- src/components/Galleries.tsx
+++ src/components/Galleries.tsx
@@ -8,7 +8,13 @@ const galleryImages = [
   "/gallery (3).jpg",
   "/gallery (4).jpg",
   "/gallery (5).jpg",
-  "/gallery (6).jpg"
+  "/gallery (6).jpg",
+  "/gallery (7).jpg",
+  "/gallery (8).jpg",
+  "/gallery (9).jpg",
+  "/gallery (10).jpg",
+  "/gallery (11).jpg",
+  "/gallery (12).jpg",
 ];
 
 export default function Galleries() {
